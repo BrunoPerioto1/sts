6424995b2edef7266274ba5c09bb356e7c171d72
brunoperioto-gbmtech <bruno.perioto@gbmlogistica.com.br>
2025-09-13 00:26:47 -0300

    fix: update baseURL in apiClient to use production endpoint

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const apiClient = () => {
 
-  const baseURL = 'http://localhost:4000/';
+  const baseURL = 'https://vtsbackend.vercel.app/';
 
 
   const dashboard = axios.create({
